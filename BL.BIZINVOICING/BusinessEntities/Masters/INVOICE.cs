68c944c30d60d9411f019dafa19a9c83bfdc8ce8
Maria-bsl <maria@bizlogicsolutions.com>
2024-07-31 08:22:22 +0300

    yesterday fix

--- BL.BIZINVOICING/BusinessEntities/Masters/INVOICE.cs
+++ BL.BIZINVOICING/BusinessEntities/Masters/INVOICE.cs
@@ -1220,7 +1220,7 @@ namespace BL.BIZINVOICING.BusinessEntities.Masters
                                     daily_count = (int)c.daily_count,
                                     approval_status = c.approval_status,
                                     approval_date = approval_date,
-                                    AuditBy = c.posted_by
+                                    
 
                                 }).ToList();
                 if (adetails != null && adetails.Count > 0)

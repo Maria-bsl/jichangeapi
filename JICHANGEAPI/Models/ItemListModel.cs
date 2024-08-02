0570385831232758630f3b2e646c8e55ece93d66
Maria-bsl <maria@bizlogicsolutions.com>
2024-08-02 13:24:46 +0300

    Start Setup Statistic

---
+++ JICHANGEAPI/Models/ItemListModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JichangeApi.Models
+{
+    public class ItemListModel
+    {
+
+        public string Name { get; set; }
+        public string Statistic { get; set; }
+    }
+}
---
+++ JICHANGEAPI/Models/SessionBModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JichangeApi.Models
+{
+    public class SessionBModel
+    {
+        [Required(ErrorMessage ="Missing Session ID", AllowEmptyStrings =false)]
+        public string sessB { get; set; }
+    }
+}

3c5f99c8bc175527caadc57d369cda6cd346d0f8
Maria-bsl <maria@bizlogicsolutions.com>
2024-08-07 17:20:08 +0300

    Delivery Part

---
+++ JICHANGEAPI/Models/SingletonDeliveryCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JichangeApi.Models
+{
+    public class SingletonDeliveryCode
+    {
+
+        [Required(ErrorMessage = "Missing code", AllowEmptyStrings = false)]
+        public long? code { get; set; }
+        [Required(ErrorMessage = "Missing mobile number", AllowEmptyStrings = false)]
+        public string  mobile_no { get; set; }
+    }
+}
